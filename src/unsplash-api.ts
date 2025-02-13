b3245384b5de0fbf764d73bf0cf0bb9b408a90c6
Veronika Voitovska <v.voitovska@gmail.com>
2025-02-13 09:31:03 +0200

    fix fix

---
+++ src/unsplash-api.ts
@@ -0,0 +1,53 @@
+import axios, { AxiosResponse } from "axios";
+
+interface Photo {
+  id: string;
+  created_at: string;
+  updated_at: string;
+  width: number;
+  height: number;
+  color: string;
+  description: string | null;
+  alt_description: string | null;
+  urls: {
+    raw: string;
+    full: string;
+    regular: string;
+    small: string;
+    thumb: string;
+  };
+  user: {
+    id: string;
+    username: string;
+    name: string;
+    portfolio_url: string | null;
+    profile_image: {
+      small: string;
+      medium: string;
+      large: string;
+    };
+  };
+}
+
+interface UnsplashResponse {
+  results: Photo[];
+  total: number;
+  total_pages: number;
+}
+
+export default async function fetchImages(
+  query: string,
+  page: number = 1
+): Promise<AxiosResponse<UnsplashResponse>> {
+  const response = await axios.get("https://api.unsplash.com/search/photos", {
+    params: {
+      query,
+      page,
+      orientation: "landscape",
+    },
+    headers: {
+      Authorization: "Client-ID lzZlUtJ9rgPMeD-YMKvtDmBwlCLcoalpHho1u5gg_KA",
+    },
+  });
+  return response;
+}
